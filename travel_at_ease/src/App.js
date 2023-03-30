f80b6a47f5911ff794446a1d3322c901e9c3e11f
Ranjan Yadav <113471389+Ranjan095@users.noreply.github.com>
2023-03-30 22:15:52 +0530

    footer updated

--- travel_at_ease/src/App.js
+++ travel_at_ease/src/App.js
@@ -3,12 +3,14 @@
 
 import { Navbar } from './Components/Navbar';
 import { MainRoutes } from './Components/MainRoutes';
+import Footer from './Components/Footer';
 
 function App() {
   return (
     <div>
       <Navbar />
       <MainRoutes />
+      <Footer/>
     </div>
   );
 }
